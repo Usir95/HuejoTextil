2c6544296d339e3c6b889f833fb80bafa47d2ea7
Osiris <Usir95.hp@gmail.com>
2025-07-02 01:07:18 -0600

    Separar y finalizar estilos del componente AgGrid

--- resources/js/Components/Dependencies/AgGrid.vue
+++ resources/js/Components/Dependencies/AgGrid.vue
@@ -1,10 +1,11 @@
 <template>
     <AgGridVue
-        class="ag-theme-quartz w-full"
+        :class="`${isDarkMode ? 'ag-theme-quartz-dark' : 'ag-theme-quartz'} w-full px-3 py-2`"
         :style="`height: ${height}`"
         :rowData="rowData"
         :columnDefs="columnDefs"
         :defaultColDef="defaultColDef"
+        :animateRows="true"
         :pagination="pagination"
         :paginationPageSize="paginationPageSize"
         :suppressMenuHide="true"
@@ -15,8 +16,8 @@
 </template>
 
 <script setup>
+import { ref, watch, onMounted } from 'vue'
 import { AgGridVue } from 'ag-grid-vue3'
-import { ref, watch } from 'vue'
 import { ModuleRegistry } from 'ag-grid-community'
 import {
     ClientSideRowModelModule,
@@ -28,7 +29,11 @@ import {
     RowSelectionModule,
     ColumnAutoSizeModule,
     RowStyleModule,
+    NumberEditorModule,
+    TextEditorModule ,
 } from 'ag-grid-community'
+
+import '../../../css/ag-grid-theme.css'
 
 ModuleRegistry.registerModules([
     ClientSideRowModelModule,
@@ -39,91 +44,70 @@ ModuleRegistry.registerModules([
     PaginationModule,
     RowSelectionModule,
     ColumnAutoSizeModule,
-    RowStyleModule
+    RowStyleModule,
+    NumberEditorModule,
+    TextEditorModule,
 ])
 
-// Props
 const props = defineProps({
     initialRowData: { type: Array, required: true },
     initialColumnDefs: { type: Array, default: () => [] },
     pagination: { type: Boolean, default: true },
     paginationPageSize: { type: Number, default: 50 },
     height: { type: String, default: '80vh' },
-    // headerColor: { type: String, default: 'bg-sky-800' },
+    headerColor: { type: String, default: 'bg-sky-800' }
 })
 
-// Emits
 const emit = defineEmits(['cell-clicked'])
 
-// Estado
+const isDarkMode = ref(false)
 const rowData = ref(props.initialRowData)
 const columnDefs = ref(
     props.initialColumnDefs.length
         ? props.initialColumnDefs
         : [
-            {
-                headerName: 'Datos',
-                field: 'json',
-                valueGetter: (params) => JSON.stringify(params.data),
-                flex: 1,
-                filter: false,
-                sortable: false,
-            },
-        ]
+              {
+                  headerName: 'Datos',
+                  field: 'json',
+                  valueGetter: (params) => JSON.stringify(params.data),
+                  flex: 1,
+                  filter: false,
+                  sortable: false
+              }
+          ]
 )
 
-// Columnas comunes
+const rowClassRules = ref({
+    'row-error': (params) => params.data?.estado === 'error'
+})
+
 const defaultColDef = {
+    editable: false,
     sortable: true,
     resizable: true,
     filter: true,
     floatingFilter: true,
-    flex: 1,
-    // headerClass: `${props.headerColor} text-white uppercase`,
+    flex: 1
 }
 
-// Reglas visuales de filas alternadas (estilo zebra)
-const rowClassRules = {
-    // 'ag-row-odd': (params) => params.node.rowIndex % 2 === 1,
-}
-
-// Reactividad
 watch(() => props.initialRowData, (val) => (rowData.value = val))
 watch(() => props.initialColumnDefs, (val) => {
     if (val.length) columnDefs.value = val
 })
 
-// Evento
-function emitCellClicked(event) {
+const emitCellClicked = (event) => {
     emit('cell-clicked', event)
 }
 
-// Autoajuste
-function onGridReady(params) {
+const onGridReady = (params) => {
     params.api.sizeColumnsToFit()
 }
+
+onMounted(() => {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)')
+    isDarkMode.value = prefersDark.matches
+    prefersDark.addEventListener('change', (e) => {
+        isDarkMode.value = e.matches
+    })
+})
 </script>
-
-<style>
-.ag-theme-quartz {
-    --ag-background-color: #0F172A;
-    --ag-header-cell-font-family: 'Patrick Hand', sans-serif;
-    --ag-header-background-color: #075985;
-    --ag-header-cell-hover-background-color: #08679b;
-    --ag-header-foreground-color: #075985;
-
-    --ag-header-row-height: 42px;
-    --ag-header-font-size: 14px;
-    --ag-header-font-weight: 600;
-    --ag-foreground-color: #F8FAFC;
-
-    --ag-accent-color: #f838ef;
-    --ag-odd-row-background-color: #1C2C37F0;
-    --ag-border-color: #2c3e50;
-    --ag-row-border-width: 1px;
-    --ag-font-family: 'Fredoka', sans-serif;
-}
-
-
-
-</style>
