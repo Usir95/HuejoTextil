70e6ee156c06ed8658f69cca48d63a98036bec67
Ernesto <nexiriley@gmail.com>
2025-07-10 00:04:35 -0600

    Catalogos

---
+++ database/seeders/ColoresSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ColoresSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
