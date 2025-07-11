19d5b73f1279a3000d8e0461b96a1502c5a47d05
Ernesto <nexiriley@gmail.com>
2025-07-10 21:09:16 -0600

    Migracion

---
+++ app/Http/Controllers/ClientesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Inertia\Inertia;
+
+
+class ClientesController extends Controller {
+
+    public function index() {
+        //$items = Model::get();
+        //return Inertia::render('Ruta/Index', compact('items'));
+    }
+
+    public function MyFunction(Request $request) {
+        return "MyFunction";
+    }
+}
