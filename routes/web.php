999fe6f6537e3757a0746b1f5845c36cdaa6d9cf
Osiris <usir95.hp@gmail.com>
2026-01-21 14:23:49 -0600

    tic

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::middleware([
 
     Route::get('/dashboard', function () {
         // === Bloqueo automático el 15 de noviembre ===
-        $fechaLimite = Carbon::create(2026, 01, 15, 23, 59, 59);
+        $fechaLimite = Carbon::create(2026, 02, 15, 23, 59, 59);
         if (now()->greaterThan($fechaLimite)) {
             abort(403, 'Licencia expirada. Contacta al administrador.');
         }
